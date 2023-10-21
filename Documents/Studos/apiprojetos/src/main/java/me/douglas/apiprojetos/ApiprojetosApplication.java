84f34489103df18c3f97a72ec91944d1036dc0a3
Douglascrc <douglasbuscam@gmail.com>
2023-10-21 18:01:40 -0300

    projeto

---
+++ Documents/Studos/apiprojetos/src/main/java/me/douglas/apiprojetos/ApiprojetosApplication.java
@@ -0,0 +1,13 @@
+package me.douglas.apiprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiprojetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiprojetosApplication.class, args);
+	}
+
+}
---
+++ Documents/Studos/apiprojetos/src/test/java/me/douglas/apiprojetos/ApiprojetosApplicationTests.java
@@ -0,0 +1,13 @@
+package me.douglas.apiprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiprojetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
